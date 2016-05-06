1915cd15f87b87ced8b857aa5b34ee8e07a37f8e
subhamounika <sb201@zips.uakron.edu>
2016-05-06 16:15:00 -0400

    Delete old code

--- src/Customer.cpp
+++ src/Customer.cpp
@@ -80,25 +80,7 @@ std::string Customer::statement() const {
         result += "\t";
 
         double thisAmount = 0;
-        switch(it->getVideo().getCode()) {
-
-            case Video::REGULAR:
-            thisAmount += 2;
-            if (it->getDaysRented() > 2)
-                thisAmount += (it->getDaysRented() - 2) * 1.5;
-            break;
-
-            case Video::NEW_RELEASE:
-            thisAmount += it->getDaysRented() * 3;
-            break;
-
-            case Video::CHILDRENS:
-            thisAmount += 1.5;
-            if (it->getDaysRented() > 3)
-                thisAmount += (it->getDaysRented() - 3) * 1.5;
-            break;
-        }
-
+        
         // amount of rental
         std::ostringstream out_str_stream;
         out_str_stream << thisAmount;
